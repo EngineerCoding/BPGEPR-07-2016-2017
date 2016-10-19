28a32e81f616f58b12310ac8ed18e73db7f51549
EngineerCoding <engineercoding@gmail.com>
2016-10-19 20:28:39 +0200

    Reformat EiwitReactiesOphalen

--- EiwitReactiesOphalen.py
+++ EiwitReactiesOphalen.py
@@ -1,65 +1,90 @@
-import urllib
+try:
+    import urllib.request as urllib
+except ImportError:
+    import urllib
 from re import split
 import json
-        
-def reaction_number(rn_geneid_list):
+
+
+def reaction_number(geneid_list):
     """
-    Every geneId in the rn_geneid_list will be used to find all of the
+    Every geneId in the geneid_list will be used to find all of the
     reactions.
 
-    rn_dict_reactions = dictionary with geneID as key and all of the reaction
-    numbers(rn_list_reactions) as a value.
-    rn_htmlfile = link to KEGG page that contains all of the reactions per enzyme
+    dict_reactions = dictionary with geneID as key and all of the reaction
+    numbers(list_reactions) as a value.
+    htmlfile = link to KEGG page that contains all of the reactions per enzyme
     """
-    rn_dict_reactions = {} 
-    for x in range(len(rn_geneid_list)):
-        rn_htmlfile = urllib.urlopen("http://www.genome.jp/dbget-bin/get_linkdb?-t+reaction+asn:" + str(rn_geneid_list[x]))
-        rn_htmltext = rn_htmlfile.read().decode()
-        rn_htmlfile.close()
-        rn_list_reactions = search_reactionnr(rn_htmltext)
-        for i in range(len(rn_list_reactions)):
-            rn_dict_reactions.update({rn_geneid_list[x]: rn_list_reactions})
-    return rn_dict_reactions
+    dict_reactions = {}
+    for x in range(len(geneid_list)):
+        htmlfile = urllib.urlopen("http://www.genome.jp/dbget-bin/get_linkdb?"
+                                  "-t+reaction+asn:" + str(geneid_list[x]))
+        htmltext = htmlfile.read().decode()
+        htmlfile.close()
+        list_reactions = search_reactionnr(htmltext)
+        for i in range(len(list_reactions)):
+            dict_reactions.update({geneid_list[x]: list_reactions})
+    return dict_reactions
 
 
-def search_reactionnr(sr_htmltext):
-    sr_tag = 0
-    sr_collection = ""
-    sr_all_reactions = []
-    sr_reaction_nr = ""
-    for sr_kar in sr_htmltext:
-        if sr_kar == '>' and sr_tag == 0:
-            sr_collection = ""
-        else :
-            sr_collection += sr_kar
-        if sr_collection == 'R' and sr_tag == 0:
-            sr_tag = 1
-        sr_tag, sr_reaction_nr, sr_all_reactions = tag_reactionnr(sr_kar, sr_tag,
-                                                        sr_all_reactions, sr_reaction_nr)
-    return sr_all_reactions
+def search_reactionnr(htmltext):
+    """ Searches for the reaction numbers in the html text
 
-
-def tag_reactionnr(tr_char, tr_tag, tr_all_reactions, tr_reaction_nr):
+    Parameters:
+        htmltext - string.
+    Returns:
+        A list of reaction numbers which can be used in the KEGG API.
     """
-    Whenever a specific part in the html code is tagged (tr_tag = 1), add 
-    all of the following characters ( tr_char) to the string tr_reaction_nr.
+    tag = 0
+    collection = ""
+    all_reactions = []
+    reaction_nr = ""
+    for char in htmltext:
+        if char == '>' and tag == 0:
+            collection = ""
+        else:
+            collection += char
+        if collection == 'R' and tag == 0:
+            tag = 1
+        tag, reaction_nr, all_reactions = tag_reactionnr(char, tag,
+                                                         all_reactions,
+                                                         reaction_nr)
+    return all_reactions
 
-    tr_tag = indicates when text is tagged
-    tr_char = character in text
-    tr_reaction_nr = string containg reactionnumber
-    tr_all_reactions = contains all reaction numbers
+
+def tag_reactionnr(char, tag, all_reactions, reaction_nr):
     """
-    if tr_tag == 1 :
-        if tr_char != "<" :
-            tr_reaction_nr += tr_char
-        else :
-            tr_all_reactions.append(tr_reaction_nr)
-            tr_reaction_nr = ""
-            tr_tag = 0
-    return tr_tag, tr_reaction_nr, tr_all_reactions 
+    Whenever a specific part in the html code is tagged (tag = 1), add
+    all of the following characters ( char) to the string reaction_nr.
+
+    tag = indicates when text is tagged
+    char = character in text
+    reaction_nr = string containg reactionnumber
+    all_reactions = contains all reaction numbers
+    """
+    if tag == 1:
+        if char != "<":
+            reaction_nr += char
+        else:
+            all_reactions.append(reaction_nr)
+            reaction_nr = ""
+            tag = 0
+    return tag, reaction_nr, all_reactions
 
 
 def get_line(lines, starting):
+    """ Reads lines until it hits the starting string. Note that the read
+    line is being stripped, meaning that putting whitespace in the starting
+    string would be redundant.
+
+    Arguments:
+        lines - File like object. A file like (or url) object which can be iterated through
+        for lines.
+        starting - string. The string which the line should start with.
+    Returns:
+        The line which starts with the line. If not available it will return
+        an empty string.
+    """
     for line in lines:
         line = line.strip()
         if line.startswith(starting):
@@ -68,16 +93,13 @@ def get_line(lines, starting):
 
 
 def main():
-    """
-    m_dict_reactions_prot = contains all reaction numbers
-    m_reactions_prot = list that saves all reactions per enzyme
-    m_final_output = dictionary with geneID as key and reactions (m_reactions_prot) as values
-    m_geneid_list = list of all geneids (from ncbi) """
-    m_geneid_list = ['102381974', '102383435']
-    gene_code_rcodes = reaction_number(m_geneid_list)
+    geneid_list = ['102381974', '102383435']
+    # Get reaction R codes
+    gene_code_rcodes = reaction_number(geneid_list)
     gene_code_reaction = {}
     for genecode in gene_code_rcodes:
         gene_code_reaction[genecode] = []
+        # Get all reactions for this cdoe
         for rcode in gene_code_rcodes[genecode]:
             kegg_api = urllib.urlopen("http://rest.kegg.jp/get/reaction:{}"
                                       .format(rcode))
@@ -92,6 +114,3 @@ def main():
 
 
 main()
-
-
-
