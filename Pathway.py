a42d439eeb17738ecb2bcce755a2c8c38933cd33
EngineerCoding <engineercoding@gmail.com>
2016-10-19 20:18:01 +0200

    Add pathway documentation

--- Pathway.py
+++ Pathway.py
@@ -2,11 +2,22 @@ try:
     import urllib.request as urllib
 except ImportError:
     import urllib
-import json
 from re import split
 
 
 def get_line(lines, starting):
+    """ Reads lines until it hits the starting string. Note that the read
+    line is being stripped, meaning that putting whitespace in the starting
+    string would be redundant.
+
+    Arguments:
+        lines - File like object. A file like (or url) object which can be iterated through
+        for lines.
+        starting - string. The string which the line should start with.
+    Returns:
+        The line which starts with the line. If not available it will return
+        an empty string.
+    """
     for line in lines:
         line = line.decode().strip()
         if line.startswith(starting):
@@ -15,6 +26,14 @@ def get_line(lines, starting):
 
 
 def get_asn_for_gene(protein_code):
+    """ Does the API call to retrieve the ASN code for the protein code.
+
+    Arguments:
+        protein_code - string or int. The protein code which the API needs
+        to convert to an ASN code.
+    Returns:
+        When available, the asn code.
+    """
     connection = urllib.urlopen(
         'http://rest.kegg.jp/find/genes/' + str(protein_code))
     # Only read the first line
@@ -24,17 +43,39 @@ def get_asn_for_gene(protein_code):
 
 
 def get_pathways(asncode):
+    """ Retrieves all the asn codes for the pathway which the gene asn code
+    contains.
+
+    Parameters:
+        asncode - string. This is the asn code which is retrieved by the
+        get_asn_for_gene function.
+    Returns:
+        A list of pathway asn codes.
+    """
+    # Do the API call
     connection = urllib.urlopen('http://rest.kegg.jp/get/' + asncode)
     pathways = []
+    # Read until the line
     pathway_line = get_line(connection, 'PATHWAY')
+    # Read while an asn code is available (mutliple are)
     while pathway_line.startswith('asn'):
         pathways.append(split('\s+', pathway_line)[0])
         pathway_line = connection.readline().decode().strip()
+    # Close the connection
     connection.close()
     return pathways
 
 
 def get_authors_list(connection):
+    """ Reads the next AUTHORS line and collects all authors which are
+    associated with the publication.
+
+    Parameters:
+        connection - A file-like object. Usually this will be a connection
+        to the KEGG API.
+    Returns:
+        A list with author names
+    """
     authors = get_line(connection, 'AUTHORS')
     authors_list = authors.split(',')
     for i in range(len(authors_list)):
@@ -45,14 +86,28 @@ def get_authors_list(connection):
 
 
 def get_data_from_pathway(asn_pathway_code):
+    """ Retrieves all the data of the pathway such as name, class and all the
+     publications that are available on this pathway. This includes the
+     publication information such as title, journal, reference and authors.
+
+     Parameters:
+         asn_pathway_code - string. The asn code that is retrieved by te
+         get_pathways function.
+     Returns:
+         A dictionary with the collected information.
+    """
     connection = urllib.urlopen(
         'http://rest.kegg.jp/get/path:' + asn_pathway_code)
+    # Collect the general data of the pathway
     collected_data = {'name': get_line(connection, 'NAME'),
                       'class': get_line(connection, 'CLASS'),
                       'publications': []}
+    # Read all the publications if available
     reference_line = get_line(connection, 'REFERENCE')
     while reference_line:
+        # Retrieves the complete authors list
         publication = dict(authors=get_authors_list(connection))
+        # Retrieves other information about the publications
         publication['title'] = get_line(connection, 'TITLE')
         publication['journal'] = get_line(connection, 'JOURNAL')
         publication['id'] = reference_line.split(':')[1]
