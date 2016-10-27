65f3a4ecab20b1711bec88a3b8c8824ea988a86b
JorikTM <jorik_tm@hotmail.com>
2016-10-27 14:40:21 +0200

    remove excess whitespace

--- create_table.sql
+++ create_table.sql
@@ -13,7 +13,6 @@ CREATE TABLE Exon_07 (
   start_positie INTEGER,
   eind_positie INTEGER,
 
-
   FOREIGN KEY (gen_id) REFERENCES Gen_07(gen_id)
 );
 
@@ -29,14 +28,14 @@ CREATE TABLE Eiwit_07 (
 
 DROP TABLE IF EXISTS Reactie_07 CASCADE;
 CREATE TABLE Reactie_07 (
- 	reactie_id VARCHAR(6) PRIMARY KEY,
+  reactie_id VARCHAR(6) PRIMARY KEY,
   reactie TEXT,
   reactie_ec VARCHAR(15)
 );
 
 DROP TABLE IF EXISTS Pathway_07 CASCADE;
 CREATE TABLE Pathway_07 (
- 	pathway_id CHAR(8) PRIMARY KEY,
+  pathway_id CHAR(8) PRIMARY KEY,
   pathway_naam TEXT,
   class TEXT,
   referentie_id INTEGER
@@ -62,22 +61,21 @@ CREATE TABLE EiwitReactie_07 (
 
 DROP TABLE IF EXISTS Referentie_07 CASCADE;
 CREATE TABLE Referentie_07 (
- 	referentie_id INTEGER PRIMARY KEY,
+  referentie_id INTEGER PRIMARY KEY,
   pathway_id CHAR (8),
   titel TEXT,
   journal TEXT,
 
-
-	FOREIGN KEY (pathway_id) REFERENCES Pathway_07(pathway_id)
+  FOREIGN KEY (pathway_id) REFERENCES Pathway_07(pathway_id)
 );
 
 DROP TABLE IF EXISTS ReferentieAuteur_07 CASCADE;
 CREATE TABLE ReferentieAuteur_07 (
- 	auteur_id SERIAL,
+  auteur_id SERIAL,
   referentie_id INTEGER,
 
 
-	FOREIGN KEY (auteur_id) REFERENCES Auteur_07(auteur_id),
+  FOREIGN KEY (auteur_id) REFERENCES Auteur_07(auteur_id),
   FOREIGN KEY (referentie_id) REFERENCES Referentie_07(referentie_id)
 );
 
@@ -89,7 +87,7 @@ CREATE TABLE Auteur_07 (
 
 DROP TABLE IF EXISTS Domein_07 CASCADE;
 CREATE TABLE Domein_07 (
- 	domein_id SERIAL PRIMARY KEY,
+  domein_id SERIAL PRIMARY KEY,
   gem_domein_lengte NUMERIC(3,2),
   gem_alignment_coverage NUMERIC(3,2),
   gem_sequentie_coverage NUMERIC(3,2)
@@ -97,9 +95,9 @@ CREATE TABLE Domein_07 (
 
 DROP TABLE IF EXISTS EiwitDomein_07 CASCADE;
 CREATE TABLE EiwitDomein_07 (
- 	eiwit_id NUMERIC(9,0),
+  eiwit_id NUMERIC(9,0),
   domein_id SERIAL,
 
-	FOREIGN KEY (eiwit_id) REFERENCES Eiwit_07(eiwit_id),
+  FOREIGN KEY (eiwit_id) REFERENCES Eiwit_07(eiwit_id),
   FOREIGN KEY (domein_id) REFERENCES Domein_07(domein_id)
 );
